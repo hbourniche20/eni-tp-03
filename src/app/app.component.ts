1bdeeb70a18f654f7fe3079c9471ab82bfbd565d
hbournic2020 <h.bourniche@cosling.com>
2022-06-22 09:54:56 +0200

    tp-3 first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Entreprise} from "./Entreprise";
+import {Equipe} from "./Equipe";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp-03';
+
+  entreprise = new Entreprise("Cosling");
+
+  public ajouterEquipe(nom: string) {
+    const e = new Equipe(nom);
+    this.entreprise.addEquipe(e);
+  }
+
+  public enleverEquipe(id: number) {
+    this.entreprise.deleteEquipe(id);
+  }
+
+  enleverPersonneEquipe(equipeId: number, personneID: number) {
+    this.entreprise.deletePersonneEquipe(personneID, equipeId);
+  }
+
+  ajouterPersonne(nom: string, prenom: string, equipeId: string) {
+    this.entreprise.addMemberToTeam(nom, prenom, parseInt(equipeId));
+  }
+
+  enleverPersonne(id: number) {
+    this.entreprise.deletePersonne(id);
+  }
+
 }
